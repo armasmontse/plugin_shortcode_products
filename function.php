547d3a3baae36fd6c83f164f532c247df69d9ea3
Montse Armas <montse_armas@Mac-mini-de-Montse.local>
2017-04-18 18:40:28 -0500

    pseudocodigo

--- function.php
+++ function.php
@@ -1,5 +1,13 @@
 ?php
 
+	function create_shortcode() {  // función para crear el shortcode
+   	
+		
 
+	}
+	add_shortcode( 'last_products', 'last_products' );
+
+
+	[last_products] // este es el shortcode
 
 ?
